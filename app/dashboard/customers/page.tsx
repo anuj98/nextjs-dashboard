cb88ed55fdd062d351760d38fc7f079ad0aec363
Anuj Upadhyaya <anujupadhyaya8@gmail.com>
2024-05-12 12:09:25 +0530

    Added all the routing with Postgres data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import { fetchFilteredCustomers } from '@/app/lib/data'
+import Table from '@/app/ui/customers/table'
+import { useSearchParams } from 'next/navigation';
+
+export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const currentPage = searchParams?.page || 1;
+
+    const customers = await fetchFilteredCustomers(query);
+    return(
+        <main>
+            {/* @ts-expect-error Async Server Component*/}
+            <Table customers={customers} currentPage={currentPage} />
+        </main>
+    )
 }
